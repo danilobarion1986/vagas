cb332592d878bef34c458f14f9f807633afcd196
Alexandre Saudate <alesaudate@gmail.com>
2019-01-15 16:12:53 -0200

    fixing test profile

--- src/test/java/br/com/vagas/ws/PeopleServiceTestIT.java
+++ src/test/java/br/com/vagas/ws/PeopleServiceTestIT.java
@@ -7,13 +7,16 @@ import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.*;
 
+import br.com.vagas.VagasApplication;
 import br.com.vagas.domain.Person;
 import io.restassured.RestAssured;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
+@SpringBootTest(classes=VagasApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PeopleServiceTestIT {
     
     
@@ -40,8 +43,13 @@ public class PeopleServiceTestIT {
             .then()
             .statusCode(201)
             .and()
-            .header("Location", is(String.format("http://localhost:%d/v1/pessoas", port)))
-            
+            .header("Location", is(String.format("http://localhost:%d/v1/pessoas/1", port)))
+            .and()
+            .body("nome", is("Alexandre Saudate"))
+            .body("profissao", is("IT Specialist"))
+            .body("localizacao", is("A"))
+            .body("nivel", is(5))
+            .body("id", is(1))
             ;
             
         
